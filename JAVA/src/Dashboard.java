032340447abc52148901f9133bc7c451d60a65d7
MOUAD FIFEL <mouad.fifel5@gmail.com>
2023-05-10 23:52:45 +0100

    enter polymorphism

---
+++ JAVA/src/Dashboard.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dashboard {
+    private final List<Alarm> allAlarms = new ArrayList<>();
+
+    public void add(Alarm alarm){
+        alarm.turnOn();
+        allAlarms.add(alarm);
+    }
+    public void PrintBigReport(){
+        for(Alarm alarm:allAlarms)
+            System.out.println(alarm.getReport(true));
+    }
+
+    public static void main(String[] args) {
+        Dashboard dashboard = new Dashboard();
+
+        dashboard.add(new PrioritizedAlarm("temperator is too high", 2));
+        dashboard.add(new HighVisibilityAlarm("Pressure too low"));
+
+        dashboard.PrintBigReport();
+    }
+}
